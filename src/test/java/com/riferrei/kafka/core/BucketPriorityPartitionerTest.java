4390f0aa61c16a88a2d9e0154561619d32ce8935
riferrei <riferrei@riferrei.com>
2020-08-13 11:24:35 -0400

    Added tests to check custom fallback actions

--- src/test/java/com/riferrei/kafka/core/BucketPriorityPartitionerTest.java
+++ src/test/java/com/riferrei/kafka/core/BucketPriorityPartitionerTest.java
@@ -34,6 +34,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BucketPriorityPartitionerTest {
@@ -109,15 +110,90 @@ public class BucketPriorityPartitionerTest {
         BucketPriorityPartitioner partitioner = new BucketPriorityPartitioner();
         partitioner.configure(configs);
         // Create a topic with only one partition...
-        PartitionInfo partitionInfo = new PartitionInfo(topic, 0, null, null, null);
-        List<PartitionInfo> partitions = List.of(partitionInfo);
+        PartitionInfo partition0 = new PartitionInfo(topic, 0, null, null, null);
+        List<PartitionInfo> partitions = List.of(partition0);
         Cluster cluster = new Cluster("test", new ArrayList<Node>(),
             partitions, Set.of(), Set.of());
-        try (MockProducer<String, String> producer = new MockProducer<>(cluster, true,
-            partitioner, new StringSerializer(), new StringSerializer())) {
+        try (MockProducer<String, String> producer = new MockProducer<>(cluster,
+            true, partitioner, new StringSerializer(), new StringSerializer())) {
             assertThrows(InvalidConfigurationException.class, () -> {
                 producer.send(new ProducerRecord<String, String>(topic, "Platinum-001", "value"));
             });
+        }
+    }
+
+    @Test
+    public void checkRoundRobinFallbackAction() {
+        final String topic = "orders";
+        Map<String, String> configs = new HashMap<>();
+        configs.put(BucketPriorityConfig.TOPIC_CONFIG, topic);
+        configs.put(BucketPriorityConfig.BUCKETS_CONFIG, "Platinum, Gold");
+        configs.put(BucketPriorityConfig.ALLOCATION_CONFIG, "70, 30");
+        configs.put(BucketPriorityConfig.FALLBACK_ACTION_CONFIG, "RoundRobin");
+        BucketPriorityPartitioner partitioner = new BucketPriorityPartitioner();
+        partitioner.configure(configs);
+        // Create two partitions so the round robin algorithm can leverage
+        PartitionInfo partition0 = new PartitionInfo(topic, 0, null, null, null);
+        PartitionInfo partition1 = new PartitionInfo(topic, 1, null, null, null);
+        List<PartitionInfo> partitions = List.of(partition0, partition1);
+        Cluster cluster = new Cluster("test", new ArrayList<Node>(),
+            partitions, Set.of(), Set.of());
+        try (MockProducer<String, String> producer = new MockProducer<>(cluster,
+            true, partitioner, new StringSerializer(), new StringSerializer())) {
+            List<Integer> chosenPartitions = new ArrayList<>();
+            // Produce 10 different records
+            for (int i = 0; i < 10; i++) {
+                ProducerRecord<String, String> record =
+                    // Creating a record without key to force the fallback...
+                    new ProducerRecord<String, String>(topic, null, "value");
+                producer.send(record, (metadata, exception) -> {
+                    chosenPartitions.add(metadata.partition());
+                });
+            }
+            assertEquals(List.of(0,1,0,1,0,1,0,1,0,1), chosenPartitions);
+        }
+    }
+
+    @Test
+    public void checkDiscardFallbackAction() {
+        final String topic = "orders";
+        Map<String, String> configs = new HashMap<>();
+        configs.put(BucketPriorityConfig.TOPIC_CONFIG, topic);
+        configs.put(BucketPriorityConfig.BUCKETS_CONFIG, "Platinum, Gold");
+        configs.put(BucketPriorityConfig.ALLOCATION_CONFIG, "70, 30");
+        configs.put(BucketPriorityConfig.FALLBACK_ACTION_CONFIG, "Discard");
+        BucketPriorityPartitioner partitioner = new BucketPriorityPartitioner();
+        partitioner.configure(configs);
+        // Create two partitions to make things interesting but it doesn't matter
+        PartitionInfo partition0 = new PartitionInfo(topic, 0, null, null, null);
+        PartitionInfo partition1 = new PartitionInfo(topic, 1, null, null, null);
+        List<PartitionInfo> partitions = List.of(partition0, partition1);
+        Cluster cluster = new Cluster("test", new ArrayList<Node>(),
+            partitions, Set.of(), Set.of());
+        try (MockProducer<String, String> producer = new MockProducer<>(cluster,
+            true, partitioner, new StringSerializer(), new StringSerializer())) {
+            int counter = 0;
+            ProducerRecord<String, String> record = null;
+            List<Integer> chosenPartitions = new ArrayList<>();
+            // Produce 10 different records where 5 of them will
+            // be sent using the key "Platinum" and thus need to
+            // end up in the partition 0. The other 5 records will
+            // be sent without a key and thus... will be discarded.
+            for (int i = 0; i < 10; i++) {
+                if (++counter > 5) {
+                    record = new ProducerRecord<String, String>(topic, "Platinum", "value");
+                } else {
+                    record = new ProducerRecord<String, String>(topic, null, "value");
+                }
+                producer.send(record, (metadata, exception) -> {
+                    // Ignoring partition -1 because this means
+                    // not leveraging any available partition.
+                    if (metadata.partition() != -1) {
+                        chosenPartitions.add(metadata.partition());
+                    }
+                });
+            }
+            assertEquals(List.of(0, 0, 0, 0, 0), chosenPartitions);
         }
     }
 
